0abeb4693e43b72eb62da02e132b3ae02f91df88
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-01-02 18:18:08 +0000

    Hotfixing parallelize

--- lib/cotcube-helpers/parallelize.rb
+++ lib/cotcube-helpers/parallelize.rb
@@ -7,9 +7,9 @@ module Cotcube
     def parallelize(ary, processes: 1, threads: 1, progress: '', &block)
       chunks = []
       if [0, 1].include? processes
-        result = Parallel.map(ary, in_threads: threads) { |u, &in_thread| in_thread.call(u) }
-      elsif [0, 1].include?(threads)
-        result = Parallel.map(ary, in_processes: processes) { |u, &in_process| in_process.call(u) }
+        result = Parallel.map(ary, in_threads: threads, &block)
+      elsif [0, 1].include? threads
+        result = Parallel.map(ary, in_processes: processes, &block)
       else
         ary.each_slice(threads) { |chunk| chunks << chunk }
         result = if progress == ''
