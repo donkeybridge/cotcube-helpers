88ec7ae6aa1aaa7ba21530ffe84b8a0e64443123
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-02-01 12:55:58 +0000

    adding #deep_freeze to Enumerable

--- lib/cotcube-helpers/enum_ext.rb
+++ lib/cotcube-helpers/enum_ext.rb
@@ -11,3 +11,25 @@ class Enumerator
     ret
   end
 end
+
+# Recursively freeze self if it's Enumerable
+# Supports all Ruby versions 1.8.* to 2.2.*+
+module Kernel
+  alias deep_freeze freeze
+  alias deep_frozen? frozen?
+end
+
+# Adding deep_freeze and deep_frozen?
+module Enumerable
+  def deep_freeze
+    unless @deep_frozen
+      each(&:deep_freeze)
+      @deep_frozen = true
+    end
+    freeze
+  end
+
+  def deep_frozen?
+    !!@deep_frozen
+  end
+end
