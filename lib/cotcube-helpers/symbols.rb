068138fd8b3a8762a1bf1e2e6e50a9390d5c9fcc
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-10-28 19:47:56 +0000

    added Helpers.micros to symbols.rb
    added :id based selection to symbols and micros
    
    added return statement to tapped selectors

--- lib/cotcube-helpers/symbols.rb
+++ lib/cotcube-helpers/symbols.rb
@@ -4,7 +4,7 @@ module Cotcube
   # Missing top level documentation
   module Helpers
 
-  def symbols(config: init, type: nil, symbol: nil)
+  def symbols(config: init, type: nil, symbol: nil, id: nil)
       if config[:symbols_file].nil?
         SYMBOL_EXAMPLES
       else
@@ -13,10 +13,25 @@ module Cotcube
           .map{|row| row.to_h }
           .map{|row| [ :ticksize, :power, :bcf ].each {|z| row[z] = row[z].to_f}; row[:format] = "%#{row[:format]}f"; row }
           .reject{|row| row[:id].nil? }
-          .tap{|all| all.select!{|x| x[:type] == type} unless type.nil? }
-          .tap { |all| all.select! { |x| x[:symbol] == symbol } unless symbol.nil? }
+          .tap { |all| all.select!{|x| x[:type] == type} unless type.nil? }
+          .tap { |all| (return all.select { |x| x[:symbol] == symbol }.first ) unless symbol.nil?  }
+          .tap{ |all| (return all.select { |x| x[:id]     == id     }.first) unless     id.nil? }
       end
     end
 
+  def micros(config: init, symbol: nil, id: nil)
+    if config[:micros_file].nil?
+      MICRO_EXAMPLES
+    else
+      CSV
+        .read(config[:micros_file], headers: %i{ id symbol ticksize power months type bcf reports format name})
+        .map{|row| row.to_h }
+        .map{|row| [ :ticksize, :power, :bcf ].each {|z| row[z] = row[z].to_f}; row[:format] = "%#{row[:format]}f"; row }
+        .reject{|row| row[:id].nil? }
+        .tap{ |all| (return all.select { |x| x[:symbol] == symbol }.first) unless symbol.nil? }
+        .tap{ |all| (return all.select { |x| x[:id]     == id     }.first) unless     id.nil? }
+    end
   end
+  end
+
 end
