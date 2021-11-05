20615df3ef36470421f1bfdeee4f043f0dde6078
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-11-05 10:56:17 +0000

    added headers (:ib_symbol, :internal, :exchange, :currency) to symbol headers as well as symbol examples

--- lib/cotcube-helpers/constants.rb
+++ lib/cotcube-helpers/constants.rb
@@ -3,13 +3,13 @@
 module Cotcube
   module Helpers
     SYMBOL_EXAMPLES = [
-      { id: '13874U', symbol: 'ES', ticksize: 0.25, power: 12.5, months: 'HMUZ', bcf: 1.0, reports: 'LF', format: '8.2f', name: 'S&P 500 MICRO' },
-      { id: '209747', symbol: 'NQ', ticksize: 0.25, power: 5.0,  monhts: 'HMUZ', bcf: 1.0, reports: 'LF', format: '8.2f', name: 'NASDAQ 100 MICRO' }
+      { id: '13874U', symbol: 'ES', ib_symbol: 'ES', internal: 'ES', exchange: 'GLOBEX', currency: 'USD', ticksize: 0.25, power: 12.5, months: 'HMUZ', bcf: 1.0, reports: 'LF', format: '8.2f', name: 'S&P 500 MICRO' },
+      { id: '209747', symbol: 'NQ', ib_symbol: 'NQ', internal: 'NQ', exchange: 'GLOBEx', currency: 'USD', ticksize: 0.25, power: 5.0,  monhts: 'HMUZ', bcf: 1.0, reports: 'LF', format: '8.2f', name: 'NASDAQ 100 MICRO' }
     ].freeze
 
     MICRO_EXAMPLES = [
-      { id: '13874U', symbol: 'ET', ticksize: 0.25, power: 1.25, months: 'HMUZ', bcf: 1.0, reports: 'LF', format: '8.2f', name: 'MICRO S&P 500 MICRO' },
-      { id: '209747', symbol: 'NM', ticksize: 0.25, power: 0.5,  monhts: 'HMUZ', bcf: 1.0, reports: 'LF', format: '8.2f', name: 'MICRO NASDAQ 100 MICRO' }
+      { id: '13874U', symbol: 'ET', ib_symbol: 'MES', internal: 'MES', exchange: 'GLOBEX', currency: 'USD', ticksize: 0.25, power: 1.25, months: 'HMUZ', bcf: 1.0, reports: 'LF', format: '8.2f', name: 'MICRO S&P 500 MICRO' },
+      { id: '209747', symbol: 'NM', ib_symbol: 'MNQ', internal: 'MNQ', exchange: 'GLOBEX', currency: 'USD', ticksize: 0.25, power: 0.5,  monhts: 'HMUZ', bcf: 1.0, reports: 'LF', format: '8.2f', name: 'MICRO NASDAQ 100 MICRO' }
     ].freeze
 
     COLORS         = %i[light_red light_yellow light_green red yellow green cyan magenta blue].freeze
--- lib/cotcube-helpers/symbols.rb
+++ lib/cotcube-helpers/symbols.rb
@@ -4,34 +4,44 @@ module Cotcube
   # Missing top level documentation
   module Helpers
 
-  def symbols(config: init, type: nil, symbol: nil, id: nil)
+    def symbols(config: init, type: nil, symbol: nil, id: nil)
       if config[:symbols_file].nil?
         SYMBOL_EXAMPLES
       else
         CSV
-          .read(config[:symbols_file], headers: %i{ id symbol ticksize power months type bcf reports format name})
+          .read(config[:symbols_file], headers: %i{ id symbol ib_symbol internal exchange currency ticksize power months type bcf reports format name})
           .map{|row| row.to_h }
-          .map{|row| [ :ticksize, :power, :bcf ].each {|z| row[z] = row[z].to_f}; row[:format] = "%#{row[:format]}f"; row }
+          .map{|row|
+            [ :ticksize, :power, :bcf ].each {|z| row[z] = row[z].to_f}
+            row[:format] = "%#{row[:format]}f"
+            row[:currency] ||= 'USD'
+            row
+          }
           .reject{|row| row[:id].nil? }
-          .tap { |all| all.select!{|x| x[:type] == type} unless type.nil? }
-          .tap { |all| (return all.select { |x| x[:symbol] == symbol }.first ) unless symbol.nil?  }
+          .tap{ |all| all.select!{|x| x[:type] == type} unless type.nil? }
+          .tap{ |all| (return all.select { |x| x[:symbol] == symbol }.first ) unless symbol.nil?  }
           .tap{ |all| (return all.select { |x| x[:id]     == id     }.first) unless     id.nil? }
       end
     end
 
-  def micros(config: init, symbol: nil, id: nil)
-    if config[:micros_file].nil?
-      MICRO_EXAMPLES
-    else
-      CSV
-        .read(config[:micros_file], headers: %i{ id symbol ticksize power months type bcf reports format name})
-        .map{|row| row.to_h }
-        .map{|row| [ :ticksize, :power, :bcf ].each {|z| row[z] = row[z].to_f}; row[:format] = "%#{row[:format]}f"; row }
-        .reject{|row| row[:id].nil? }
-        .tap{ |all| (return all.select { |x| x[:symbol] == symbol }.first) unless symbol.nil? }
-        .tap{ |all| (return all.select { |x| x[:id]     == id     }.first) unless     id.nil? }
+    def micros(config: init, symbol: nil, id: nil)
+      if config[:micros_file].nil?
+        MICRO_EXAMPLES
+      else
+        CSV
+          .read(config[:micros_file], headers: %i{ id symbol ib_symbol internal exchange currency ticksize power months type bcf reports format name})
+          .map{|row| row.to_h }
+          .map{|row|
+            [ :ticksize, :power, :bcf ].each {|z| row[z] = row[z].to_f }
+            row[:format] = "%#{row[:format]}f"
+            row[:currency] ||= 'USD'
+            row
+          }
+          .reject{|row| row[:id].nil? }
+          .tap{ |all| (return all.select { |x| x[:symbol] == symbol }.first) unless symbol.nil? }
+          .tap{ |all| (return all.select { |x| x[:id]     == id     }.first) unless     id.nil? }
+      end
     end
-  end
   end
 
 end
