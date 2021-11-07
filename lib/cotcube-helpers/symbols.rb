d6ec11a8b5cb7635cd48b5f0897ba327930c46ba
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-11-07 16:57:21 +0000

    symbols: made selection of symbols more versatile by key

--- lib/cotcube-helpers/symbols.rb
+++ lib/cotcube-helpers/symbols.rb
@@ -4,23 +4,34 @@ module Cotcube
   # Missing top level documentation
   module Helpers
 
-    def symbols(config: init, type: nil, symbol: nil, id: nil)
+    SYMBOL_HEADERS = %i[ id symbol ib_symbol internal exchange currency ticksize power months type bcf reports format name ]
+
+    def symbols(config: init, **args)
       if config[:symbols_file].nil?
         SYMBOL_EXAMPLES
       else
         CSV
-          .read(config[:symbols_file], headers: %i{ id symbol ib_symbol internal exchange currency ticksize power months type bcf reports format name})
+          .read(config[:symbols_file], headers: SYMBOL_HEADERS)
           .map{|row| row.to_h }
           .map{|row|
             [ :ticksize, :power, :bcf ].each {|z| row[z] = row[z].to_f}
             row[:format] = "%#{row[:format]}f"
             row[:currency] ||= 'USD'
+            row[:multiplier] = (row[:ticksize] / row[:power]).round(8)
             row
           }
           .reject{|row| row[:id].nil? }
-          .tap{ |all| all.select!{|x| x[:type] == type} unless type.nil? }
-          .tap{ |all| (return all.select { |x| x[:symbol] == symbol }.first ) unless symbol.nil?  }
-          .tap{ |all| (return all.select { |x| x[:id]     == id     }.first) unless     id.nil? }
+          .tap{ |all|
+            args.keys.each { |header|
+              unless SYMBOL_HEADERS.include? header
+                puts "WARNING in Cotcube::Helpers.symbols: '#{header}' is not a valid symbol header. Skipping..."
+                next
+              end
+              all.select!{|x| x[header] == args[header]} unless args[header].nil?
+              return all.first if all.size == 1
+            }
+            return all
+          }
       end
     end
 
@@ -29,7 +40,7 @@ module Cotcube
         MICRO_EXAMPLES
       else
         CSV
-          .read(config[:micros_file], headers: %i{ id symbol ib_symbol internal exchange currency ticksize power months type bcf reports format name})
+          .read(config[:micros_file], headers: SYMBOL_HEADERS)
           .map{|row| row.to_h }
           .map{|row|
             [ :ticksize, :power, :bcf ].each {|z| row[z] = row[z].to_f }
@@ -38,8 +49,17 @@ module Cotcube
             row
           }
           .reject{|row| row[:id].nil? }
-          .tap{ |all| (return all.select { |x| x[:symbol] == symbol }.first) unless symbol.nil? }
-          .tap{ |all| (return all.select { |x| x[:id]     == id     }.first) unless     id.nil? }
+          .tap{ |all|
+            args.keys.each { |header|
+             unless SYMBOL_HEADERS.include? header
+                puts "WARNING in Cotcube::Helpers.symbols: '#{header}' is not a valid symbol header. Skipping..."
+                next
+              end
+              all.select!{|x| x[header] == args[header]} unless args[header].nil?
+              return all.first if all.size == 1
+            }
+            return all
+          }
       end
     end
   end
