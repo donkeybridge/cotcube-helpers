7a744691e29e313f20381a32a3e7267c4d70b74c
Benjamin L. Tischendorf <donkeybridge@jtown.eu>
2021-12-23 08:53:22 +0000

    hash_ext: reworked keys_to_sym!

--- lib/cotcube-helpers/hash_ext.rb
+++ lib/cotcube-helpers/hash_ext.rb
@@ -2,15 +2,15 @@
 
 # Monkey patching the Ruby Core class Hash
 class Hash
-  def keys_to_sym
-    each_key do |key|
+  def keys_to_sym!
+    self.keys.each do |key|
       case self[key].class.to_s
       when 'Hash'
-        self[key].keys_to_sym
+        self[key].keys_to_sym!
       when 'Array'
-        self[key].map { |el| el.is_a?(Hash) ? el.keys_to_sym : el }
+        self[key].map { |el| el.is_a?(Hash) ? el.keys_to_sym! : el }
       end
-      self[key.to_sym] = delete(key)
+      self["#{key}".to_sym] = delete(key)
     end
     self
   end
